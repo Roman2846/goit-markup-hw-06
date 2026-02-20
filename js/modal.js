2542181bd2de05d954625fd9a974bd00156ad3e2
Roman <krasivskyi01@gmail.com>
2026-02-20 12:46:20 +0200

    hm6

--- js/modal.js
+++ js/modal.js
@@ -16,3 +16,22 @@
     refs.modal.classList.toggle("is-open");
   }
 })();
+
+(() => {
+  const refs = {
+    // Додати атрибут data-modal-menu-open на кнопку відкриття
+    openModalBtn: document.querySelector("[data-modal-menu-open]"),
+    // Додати атрибут data-modal-menu-close на кнопку закриття
+    closeModalBtn: document.querySelector("[data-modal-menu-close]"),
+    // Додати атрибут data-menu-modal на бекдроп модалки
+    modal: document.querySelector("[data-menu-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
+    refs.modal.classList.toggle("is-open");
+  }
+})();
